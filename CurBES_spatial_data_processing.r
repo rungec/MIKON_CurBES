8d00612149153120417874649c69e5056f17b3d8
rungec <cru016@uit.no>
2019-04-17 14:08:55 +0200

    added corrine and pca

--- CurBES_spatial_data_processing.r
+++ CurBES_spatial_data_processing.r
@@ -3,12 +3,14 @@
 require(sf)
 require(tidyverse)
 
-wd <- 
-setwd(wd)
+#wd <- 
+#setwd(wd)
 
 
 ###load data ----
 bb <- st_read("D:/Box Sync/Arctic/Data/Boundaries/Borders/By_country/Norway", "North_municipalities_dissolve")
+laea_crs <- st_read("D:/Box Sync/Arctic/Data/Boundaries/Borders/By_country/Norway", "AdmBoudariesNorway_lambert") %>%
+            st_crs()#for the laea polar crs
 
 ppgis <- st_read("D:/Box Sync/Arctic/Data/PPGIS_CultES/Processed/shps", "PPGIS_Markers_north_UTM33N") %>% 
           st_intersection(bb) %>%
@@ -16,14 +18,14 @@ ppgis <- st_read("D:/Box Sync/Arctic/Data/PPGIS_CultES/Processed/shps", "PPGIS_M
                                  "hunt/fish", "income", "otherchange", "pasture", "recreation", 
                                  "scenic", "social", "specialplace", "spiritual", "therapuetic", 
                                  "undisturbnature")) #drop the + and - categories
-participants <- ""
 
 roads <- st_read("O:/Claire_Big/Arctic/Original/BasemapN500", "n500_roadsl") %>% 
             st_intersection(bb) %>%
             filter(OBJTYPE %in% c("VegSenterlinje", "Bane")) #drop tracks and tractor paths
 corrine <- st_read("O:/Claire_Big/Arctic/Original/CORINE2012", "CORINE2012_Norge_ab21a") %>% 
             st_transform(st_crs(ppgis)) %>%
-            st_intersection(bb)
+            st_intersection(bb) #%>%
+            #st_transform(laea_crs)
 elevation <- st_read("O:/Claire_Big/Arctic/Original/BasemapN500", "n500_elevation layersf") %>% 
             st_intersection(bb) %>%
             select(MINHOEYDE) #select the min height column
@@ -68,26 +70,57 @@ ppgis_spatial <- ppgis_spatial %>%
                                                   dist2lake_m > 500 | dist2river_m > 500 ~FALSE))
 
 #Elevation (minimum, in 500m intervals)
-ppgis_spatial <- st_intersection(ppgis_spatial, elevation)
+elev <- st_intersection(ppgis, elevation) %>% 
+          st_set_geometry(NULL) %>% 
+          select(ID, MINHOEYDE)
 
-#PCA of corrine variables ----
+ppgis_spatial <- merge(ppgis_spatial, elev, by="ID", all.x=TRUE)
+ppgis_spatial$MINHOEYDE[is.na(ppgis_spatial$MINHOEYDE)] <- 0 #replace elevation for points falling in the ocean as elevation=0
 
 
+#Corrine land use
+#make a 1km buffer around each point and calculate it's area. nQuadSegs sets how many straightline segments to dissolve the circle quadrant into (more segments=closer to a true circle).
+#not quite as simple as pi*r^2, as the earth is not a perfect sphere, so we let r do it for us.
+pointbuff1km <- ppgis %>% 
+        #st_transform(st_crs(laea_crs)) %>% #do the calculations in equal area projection
+        st_buffer(dist=1000, nQuadSegs=100) %>%
+        mutate(circle_area = st_area(.) %>% as.numeric())
 
+#intersect corrine within 1km of each point, calculate the area of each corrine class within each buffered point
+lulc <- st_intersection(pointbuff1km, corrine) %>%
+        mutate(area = st_area(.) %>% as.numeric()) %>%
+        st_set_geometry(NULL) %>%
+        mutate(lulc_class = case_when(CLC12_KODE %in% c(311, 313)~"broadleafforest",
+                                      CLC12_KODE %in% c(312)~"coniferforest",
+                                      CLC12_KODE %in% c(321:324)~"heathshrub",
+                                      CLC12_KODE %in% c(331:335)~"sparselyvegetated",
+                                      CLC12_KODE %in% c(211, 212, 213, 221, 222, 223, 231, 241:244)~"cropland",
+                                      CLC12_KODE %in% c(411, 412, 422, 423)~"wetland",
+                                      CLC12_KODE %in% c(112,121, 123, 124, 131, 132, 133)~"developed",#developed=(urban, airports, ports, mines, dumps, construction)
+                                      CLC12_KODE %in% c(141, 142)~"urbangreen", 
+                                      CLC12_KODE %in% c(511, 512, 521:523)~"water") %>% as.factor) %>%
+        group_by(ID, lulc_class) %>%
+        summarise(#area_ha_in1km = sum(area/10000),
+                  percent_area = sum(area)/mean(circle_area)*100) %>%
+        spread(lulc_class, percent_area, fill=0)
 
-#Merge ppgis with participant characteristics ----
-ppgis_out <- merge(ppgis_spatial, participants, by="userID", all.x=TRUE)
+ppgis_spatial <- merge(ppgis_spatial, lulc, by="ID", all.x=TRUE)
+  
+#PCA of landcover variables ----
+pca.data <- ppgis_spatial %>% st_set_geometry(NULL) %>% 
+            select(c(broadleafforest, cropland, heathshrub, sparselyvegetated, cropland, 
+                     wetland, developed, urbangreen, MINHOEYDE))                                                                
+pca.envir<-princomp(pca.data, cor=T) # important to remember to normalize the data in PCA, if on different #scales to prevent that certain variables dominate, but the landcoverdata was on same scale as far as I #remember
+biplot(pca.envir)
+plot(pca.envir)
+loadings(pca.envir)
+summary(pca.envir)
+PCA.var<-as.data.frame(pca.envir$scores[,1:2]) #make data frame of PCA variables 
+names(PCA.var) <- c("PCA_comp1", "PCA_comp2", "PCA_comp3")
 
+ppgis_spatial <- ppgis_spatial %>% bind_cols(PCA.var)
 
 #Save dataset
-ppgis_out %>% st_write("D:/Box Sync/Arctic/MIKON/CurBES/Data/ppgis", "Curbes_ppgis_plus_info.shp")
-ppgis_out %>% st_geometry(NULL) %>% write_csv("D:/Box Sync/Arctic/MIKON/CurBES/Data/ppgis/Curbes_ppgis_plus_info.csv")
-
-
-quantile(d2)
-mean(d2)
-median(d2)
-length(d2[d2<1001])
-hist(d2)
-
+ppgis_out %>% st_write("D:/Box Sync/Arctic/MIKON/CurBES/Data/ppgis", "Curbes_ppgis_plus_variables.shp")
+ppgis_out %>% st_geometry(NULL) %>% write_csv("D:/Box Sync/Arctic/MIKON/CurBES/Data/ppgis/Curbes_ppgis_plus_variables.csv")
 
