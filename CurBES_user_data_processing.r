fa7c4eb8880ad7b73bf0f938e9ce3de0fe158550
rungec <cru016@uit.no>
2019-05-01 10:52:27 +0200

    more correspondence anlysis

--- CurBES_user_data_processing.r
+++ CurBES_user_data_processing.r
@@ -21,19 +21,25 @@ ca.data <- ppgis %>% st_set_geometry(NULL) %>%
                           "hunt/fish", "income", "pasture", "recreation", 
                           "scenic", "social", "specialplace", "spiritual", "therapuetic", 
                           "undisturbnature")) %>% droplevels() %>%
+  mutate(activity = case_when(category %in% c("biological", "undisturbnature")~ "nature",
+                              category %in% c("income", "pasture")~ "grazing",
+                              category %in% c("cabin", "social", "cultureident", "gathering")~ "social",
+                              category %in% c("specialplace", "spiritual", "therapuetic")~ "special",
+                              category %in% c("scenic")~ "scenic",
+                              category %in% c("recreation")~ "recreation",
+                              category %in% c("cleanwater")~ "cleanwater",
+                              category %in% c("hunt/fish")~ "hunt/fish")) %>%
   group_by(userID) %>% 
   mutate(npointsmapped = n()) %>%
-  group_by(category, add=TRUE) %>%
+  group_by(activity, add=TRUE) %>%
   summarise(npoints = n(),
             npointsmapped=mean(npointsmapped),
             proppoints=npoints/npointsmapped) %>%
   ungroup() %>%
-  #spread(key=category, value=npoints, fill=0)
-  spread(key=userID, value=proppoints, fill=0) %>%
-  select(c(userID, biological:undisturbnature))
+  select(userID, activity, proppoints) %>% 
+  spread(key=activity, value=proppoints, fill=0) 
 
-ca.values<-CA(ca.data, ncp=5, graph=FALSE) 
-
+ca.values<-CA(ca.data[,2:ncol(ca.data)], ncp=5, graph=FALSE) 
 
 png("PCA_of_uservalues.png", width=14, height=7, units="in", res=150)
 par(mfrow=c(1,2))
@@ -42,7 +48,6 @@ fviz_screeplot(ca.values)
 fviz_ca_col(ca.values, repel=TRUE)
 dev.off()
 sink("PCA_of_uservalues.txt")
-#loadings(ca.values)
 summary(ca.values)
 sink()
 
@@ -63,18 +68,62 @@ user_devpref <- ppgis %>% st_set_geometry(NULL) %>%
   group_by(userID, dev_pref) %>%
   summarise(npoints = n()) %>%
   spread(key=dev_pref, value=npoints, fill=0)
-ca.prefs<-princomp(user_devpref, cor=T) 
+
+user_devpref %>% as.data.frame() %>% write_csv("D:/Box Sync/Arctic/MIKON/CurBES/Data/ppgis/Curbes_ppgis_userdevelopmentpreferences_npoints.csv")
+
+ca.prefs<-CA(user_devpref[, 2:ncol(user_devpref)], ncp=5, graph=FALSE) 
 
 
 png("PCA_of_userpreferences.png", width=14, height=7, units="in", res=150)
 par(mfrow=c(1,2))
-biplot(ca.prefs)
-plot(ca.prefs)
+fviz_screeplot(ca.prefs)
+#fviz_ca_biplot(ca.prefs, repel=FALSE)
+fviz_ca_col(ca.prefs, repel=TRUE)
 dev.off()
 sink("PCA_of_userpreferences.txt")
-loadings(ca.prefs)
 summary(ca.prefs)
 sink()
 
-direct cluster
-redundancy analysis
+#Correspondence analysis of user values and preferences, based on the proportion of their mapped values that fall into each category
+ca.data.comb <- ppgis %>% st_set_geometry(NULL) %>% 
+  filter(category!="otherchange") %>%
+  mutate(activity = case_when(category %in% c("biological", "undisturbnature")~ "nature",
+                              category %in% c("income", "pasture")~ "grazing",
+                              category %in% c("cabin", "social", "cultureident", "gathering")~ "social",
+                              category %in% c("specialplace", "spiritual", "therapuetic")~ "special",
+                              category %in% c("scenic")~ "scenic",
+                              category %in% c("recreation")~ "recreation",
+                              category %in% c("cleanwater")~ "cleanwater",
+                              category %in% c("hunt/fish")~ "hunt/fish", 
+                              category %in% c("+fishing", "+hunting", "+grazing", "+logging")~ "+consumptive",
+                              category %in% c("-fishing", "-hunting", "-grazing", "-logging")~ "-consumptive",
+                              category %in% c("+boats", "+helicopter", "+snowmobiles", "+roads_atv")~ "+motor",
+                              category %in% c("-boats", "-helicopter", "-snowmobiles", "-roads_atv")~ "-motor",
+                              category %in% c("+development", "+energy", "+tourism")~ "+development",
+                              category %in% c("-development", "-energy", "-tourism")~ "-development",
+                              category %in% c("+predator")~ "+predator",
+                              category %in% c("-predator")~ "-predator")) %>%
+  group_by(userID) %>% 
+  mutate(npointsmapped = n()) %>%
+  group_by(activity, add=TRUE) %>%
+  summarise(npoints = n(),
+            npointsmapped=mean(npointsmapped),
+            proppoints=npoints/npointsmapped) %>%
+  ungroup() %>%
+  select(userID, activity, npoints) %>% 
+  spread(key=activity, value=npoints, fill=0) 
+
+ca.all<-CA(ca.data.comb[,2:ncol(ca.data.comb)], ncp=5, graph=FALSE) 
+
+png("PCA_of_uservaluesandprefs.png", width=14, height=7, units="in", res=150)
+par(mfrow=c(1,2))
+fviz_screeplot(ca.all)
+#fviz_ca_biplot(ca.all, repel=FALSE)
+fviz_ca_col(ca.all, repel=TRUE)
+dev.off()
+sink("PCA_of_uservaluesandprefs.txt")
+summary(ca.all)
+sink()
+
+
+
