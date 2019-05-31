ab64cadd5341bd908928c27188262d190e2965e3
rungec <c.runge1@uq.edu.au>
2019-05-31 12:32:33 +0200

    expanded model selection socio-spatial discounting

--- CurBES_spatial_discounting_model.r
+++ CurBES_spatial_discounting_model.r
@@ -394,30 +394,34 @@ ppgis_sub <- ppgis_df %>% drop_na(gender, age, education, income_NOK) %>%
 g1 <- lmer(logdist2road~ education + age + income + activity*gender + (1|LogID), data = ppgis_sub)
 #g1_1 <- lmer(dist2road_m~ education + age + income + activity*gender + (1|LogID), data = ppgis_sub) #just tried without logtransformation
 #model selection
-g1 <- lmer(logdist2road ~ education + age + income + activity*gender + (1|LogID), data = ppgis_sub)
-g2 <- lmer(logdist2road ~ education + age + activity*gender + (1|LogID), data = ppgis_sub)
-g3 <- lmer(logdist2road ~ education + income + activity*gender + (1|LogID), data = ppgis_sub)
+g1 <- lmer(logdist2road ~ age + income + education + activity*gender + (1|LogID), data = ppgis_sub)
+g2 <- lmer(logdist2road ~ age + education + activity*gender + (1|LogID), data = ppgis_sub)
+g3 <- lmer(logdist2road ~ income + education + activity*gender + (1|LogID), data = ppgis_sub)
 g4 <- lmer(logdist2road ~ education + activity*gender + (1|LogID), data = ppgis_sub)
 g5 <- lmer(logdist2road ~ activity*gender + (1|LogID), data = ppgis_sub)
 g6 <- lmer(logdist2road ~ activity + gender + (1|LogID), data = ppgis_sub)
-g7 <- lmer(logdist2road ~ activity + gender + education + (1|LogID), data = ppgis_sub)
-g8 <- lmer(logdist2road ~ activity + gender + education + age + (1|LogID), data = ppgis_sub)
-g9 <- lmer(logdist2road ~ activity + gender + education + age + income + (1|LogID), data = ppgis_sub)
-g10 <- lmer(logdist2road ~ activity + education + income + (1|LogID), data = ppgis_sub)
-g11 <- lmer(logdist2road ~ activity + education + age + (1|LogID), data = ppgis_sub)
-g12 <- lmer(logdist2road ~ activity + age + (1|LogID), data = ppgis_sub)
+g7 <- lmer(logdist2road ~ education + activity + gender + (1|LogID), data = ppgis_sub)
+g8 <- lmer(logdist2road ~ age + education + activity + gender + (1|LogID), data = ppgis_sub)
+g9 <- lmer(logdist2road ~ age + income + education + activity + gender + (1|LogID), data = ppgis_sub)
+g10 <- lmer(logdist2road ~ income + education + activity + (1|LogID), data = ppgis_sub)
+g11 <- lmer(logdist2road ~ age + education + activity + (1|LogID), data = ppgis_sub)
+g12 <- lmer(logdist2road ~ age + activity + (1|LogID), data = ppgis_sub)
+g12b <- lmer(logdist2road ~ education + activity + (1|LogID), data = ppgis_sub)
+g12c <- lmer(logdist2road ~ income + activity + (1|LogID), data = ppgis_sub)
 g13 <- lmer(logdist2road ~ activity + (1|LogID), data = ppgis_sub)
 g14 <- lmer(logdist2road ~ activity + education*gender + (1|LogID), data = ppgis_sub)
+g15 <- lmer(logdist2road ~ activity + education + (1|LogID), data = ppgis_sub)
 
-sink("Model_of_dist2road_bysocioecon_lme.txt", append=TRUE)
+sink("Model_of_dist2road_bysocioecon_lme.txt")
 print("Estimate the models by ML and find the most parsimonious")
-print(anova(g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14))
+print(anova(g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g12b, g12c, g13, g14))
 print("Full model, estimated by REML")
 print(summary(g1))
 print(car::Anova(g1)) #p values for the fixed effects
-print()
+print(" ")
 print("Most parsimonious model, estimated by REML")
 print(summary(g13))
+print(car::Anova(g13)) 
 sink()
 
 x <-as.data.frame.table(summary(g13)$coefficients) %>% spread(key = Var2, value = Freq)
@@ -435,15 +439,14 @@ write.csv(x, "Model_of_dist2road_bysocioecon_lme_fullmod_coefs.csv", row.names=F
 #QQ-plots, checks for normal distribution of residuals and homoscedasticity (constant variance of residuals) are shown https://cran.r-project.org/web/packages/sjPlot/sjPlot.pdf.
 p<-plot_model(g13, type = "diag")
 pout <- ggarrange(p[[1]], p[[2]]$LogID, p[[3]], p[[4]], ncol=2, nrow=2, labels = c("A", "B", "C", "D"))
-ggsave("Model_of_dist2road_bysocioecon_lme_diag_bestmod.png", pout, width=9, height=7, units="in", res=150)
+ggsave("Model_of_dist2road_bysocioecon_lme_diag_bestmod.png", pout, width=9, height=7, units="in", dpi=300)
 
 #plot the model diagnostics, fullmod
 p<-plot_model(g1, type = "diag")
 pout <- ggarrange(p[[1]], p[[2]]$LogID, p[[3]], p[[4]], ncol=2, nrow=2, labels = c("A", "B", "C", "D"))
-ggsave("Model_of_dist2road_bysocioecon_lme_diag_fullmod.png", pout, width=9, height=7, units="in", res=150)
+ggsave("Model_of_dist2road_bysocioecon_lme_diag_fullmod.png", pout, width=9, height=7, units="in", dpi=300)
 
 # plot marginal effects 
-png("Model_of_dist2road_bysocioecon_lme_marginaleffects_fullmod.png", width=9, height=7, units="in", res=150)
   p1 <- plot_model(g1, type = "pred", terms = "education")
   p2 <- plot_model(g1, type = "pred", terms = "age")
   p3 <- plot_model(g1, type = "pred", terms = "gender")
@@ -452,7 +455,7 @@ png("Model_of_dist2road_bysocioecon_lme_marginaleffects_fullmod.png", width=9, h
 
 pout <- ggarrange(p5, ggarrange(p1, p2, p3, p4, ncol = 2, nrow=2, labels = c("B", "C", "D", "E")), # Second row with box and dot plots
           nrow = 2, labels = "A" )   
-ggsave("Model_of_dist2road_bysocioecon_lme_marginaleffects_fullmod.png", pout, width=7, height=9, units="in", res=150)
+ggsave("Model_of_dist2road_bysocioecon_lme_marginaleffects_fullmod.png", pout, width=7, height=9, units="in", dpi=300)
 
 #plot residuals assesses how well the predicted and the observed values fit across predictors. The actual (observed) values have a coloured ???ll, while the predicted values have a solid outline without ???lling.
 #https://cran.r-project.org/web/packages/sjPlot/sjPlot.pdf
@@ -501,18 +504,21 @@ g9 <- lmer(logdist2road ~ activity + gender + education + age + income + (1|LogI
 g10 <- lmer(logdist2road ~ activity + education + income + (1|LogID), data = ppgis_sub2)
 g11 <- lmer(logdist2road ~ activity + education + age + (1|LogID), data = ppgis_sub2)
 g12 <- lmer(logdist2road ~ activity + age + (1|LogID), data = ppgis_sub2)
+g12b <- lmer(logdist2road ~ education + activity + (1|LogID), data = ppgis_sub2)
+g12c <- lmer(logdist2road ~ income + activity + (1|LogID), data = ppgis_sub2)
 g13 <- lmer(logdist2road ~ activity + (1|LogID), data = ppgis_sub2)
 g14 <- lmer(logdist2road ~ activity + education*gender + (1|LogID), data = ppgis_sub2)
 
-sink("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated.txt", append=TRUE)
+sink("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated.txt")
 print("Estimate the models by ML and find the most parsimonious")
-print(anova(g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14))
+print(anova(g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g12b, g12c, g13, g14))
 print("Full model, estimated by REML")
 print(summary(g1))
 print(car::Anova(g1)) #p values for the fixed effects
-print()
+print(" ")
 print("Most parsimonious model, estimated by REML")
 print(summary(g13))
+print(car::Anova(g13))
 sink()
 
 x <-as.data.frame.table(summary(g13)$coefficients) %>% spread(key = Var2, value = Freq)
@@ -523,15 +529,14 @@ write.csv(x, "Model_of_dist2road_bysocioecon_lme_dropprimaryeducated_fullmod_coe
 #plot the model diagnostics, bestmod
 p<-plot_model(g13, type = "diag")
 pout <- ggarrange(p[[1]], p[[2]]$LogID, p[[3]], p[[4]], ncol=2, nrow=2, labels = c("A", "B", "C", "D"))
-ggsave("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated_diag_bestmod.png", pout, width=9, height=7, units="in", res=150)
+ggsave("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated_diag_bestmod.png", pout, width=9, height=7, units="in", dpi=300)
 
 #plot the model diagnostics, fullmod
 p<-plot_model(g1, type = "diag")
 pout <- ggarrange(p[[1]], p[[2]]$LogID, p[[3]], p[[4]], ncol=2, nrow=2, labels = c("A", "B", "C", "D"))
-ggsave("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated_diag_fullmod.png", pout, width=9, height=7, units="in", res=150)
+ggsave("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated_diag_fullmod.png", pout, width=9, height=7, units="in", dpi=300)
 
 # plot marginal effects 
-png("Model_of_dist2road_bysocioecon_lme_marginaleffects_fullmod.png", width=9, height=7, units="in", res=150)
 p1 <- plot_model(g1, type = "pred", terms = "education")
 p2 <- plot_model(g1, type = "pred", terms = "age")
 p3 <- plot_model(g1, type = "pred", terms = "gender")
@@ -539,7 +544,7 @@ p4 <- plot_model(g1, type = "pred", terms = "income")
 p5 <- plot_model(g1, type = "pred", terms = "activity")
 pout <- ggarrange(p5, ggarrange(p1, p2, p3, p4, ncol = 2, nrow=2, labels = c("B", "C", "D", "E")), # Second row with box and dot plots
                   nrow = 2, labels = "A" )   
-ggsave("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated_marginaleffects_fullmod.png", pout, width=7, height=9, units="in", res=150)
+ggsave("Model_of_dist2road_bysocioecon_lme_dropprimaryeducated_marginaleffects_fullmod.png", pout, width=7, height=9, units="in", dpi=300)
 
 
 ############################
